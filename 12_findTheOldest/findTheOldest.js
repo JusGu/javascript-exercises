2871909f6f320edf39e6618435ec14504bbdd888
Justin Gu <77459359+CorsaRusso@users.noreply.github.com>
2022-05-26 13:57:13 -0400

    finished exercises 8-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = null;
+    let age = 0;
+    let cur_year = 2022;
+    for( let a = people.length - 1; a >= 0; --a ){
+        let cur_age = people[a].yearOfDeath ? people[a].yearOfDeath - people[a].yearOfBirth : cur_year - people[a].yearOfBirth;
+        oldest = cur_age > age ? people[a] : oldest;
+        age = Math.max(age, cur_age);
+    }
+    return oldest;
 
 };
 
